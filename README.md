06e36a3c680e9e923345f7f69d04fba8c1b8f5f7
Felipe Soto C <pipesoto13@gmail.com>
2020-10-27 12:57:54 -0500

    add comments

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Crea un componente
 
+Hola Maker,
+
+Tu misión en este reto es crear un componente llamado Welcome para que la aplicación en la que estamos trabajando funcione.
+
+El componente Welcome debe mostrar un h1 con el texto "Hola <name>" donde <name> es el valor del atributo que recibe Welcome. Por ejemplo
+
+<Welcome name="Pedro" />
+debería mostrar "Hola Pedro" dentro del h1.
+
+Si revisas el componente App te vas a dar cuenta que utiliza Welcome para saludar a tres personas. El resultado final debería ser el siguiente:
+
+
 Este repositorio ha sido creado con [Create React App](https://github.com/facebookincubator/create-react-app). Para resolver el reto debes realizar los siguientes pasos:
 
 1. Haz un fork de este repo.
--- src/App.js
+++ src/App.js
@@ -23,3 +23,31 @@ class App extends Component {
 }
 
 export default App;
+
+/* 
+¡Excelente trabajo Maker!
+
+Nuestra solución es la siguiente. Creamos un nuevo archivo Welcome.js con el siguiente contenido:
+
+import React from 'react';
+
+const Welcome = (props) => {
+  return <h1>Hola {props.name}</h1>
+};
+
+export default Welcome;
+Como el componente no tiene estado, lo creamos como una función. Sin embargo, también lo hubieses podido crear como una clase:
+
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return <h1>Hola {this.props.name}</h1>;
+  }
+}
+
+export default Welcome;
+Por último, debes importar el componente en el archivo App.js:
+
+import Welcome from './Welcome';
+ */
